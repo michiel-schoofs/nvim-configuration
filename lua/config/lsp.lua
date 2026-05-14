71e080b9ba3daf348c7da96cc182a014463abbd7
michiel-schoofs <36503058+michiel-schoofs@users.noreply.github.com>
2026-05-14 19:40:37 +0200

    Added inline errors, enabled auto complete

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,1 +1,40 @@
 vim.lsp.enable("rust_analyzer")
+vim.lsp.config("rust_analyzer", {
+    settings = {
+        ["rust-analyzer"] = {
+            imports = {
+                granularity = {
+                    group = "module",
+                },
+                prefix = "self",
+            },
+            cargo = {
+                buildScripts = {
+                    enable = true,
+                },
+            },
+            procMacro = {
+                enable = true
+            },
+        },
+    },
+})
+-- You can enable different LSP features
+vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(ev)
+        local client = assert(vim.lsp.get_client_by_id(ev.data.client_id))
+        -- Inlay hints display inferred types, etc.
+        if client:supports_method("inlayHint/resolve") then
+            vim.lsp.inlay_hint.enable(true, { bufnr = ev.buf })
+        end
+        -- Completion can be invoked via ctrl+x ctrl+o. It displays a list of
+        -- names inferred from the context (e.g. method names, variables, etc.)
+        if client:supports_method("textDocument/completion") then
+            vim.lsp.completion.enable(true, client.id, ev.buf, {})
+        end
+    end,
+})
+
+vim.diagnostic.config({ virtual_text = true })
+vim.cmd[[set completeopt+=menuone,noselect,popup]]
+vim.keymap.set("i", "<C-space>", vim.lsp.completion.get, { desc = "trigger autocompletion" })
